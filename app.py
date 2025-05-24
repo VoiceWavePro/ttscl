c50034547b88900f90c3c9024a50fdd21c458919
VoiceWavePro <r3895025@gmail.com>
2025-05-24 11:17:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -35,5 +35,7 @@ def generate():
         return jsonify({"error": "Audio generation failed."}), 500
 
 if __name__ == "__main__":
+    from waitress import serve
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    serve(app, host="0.0.0.0", port=port)
+
